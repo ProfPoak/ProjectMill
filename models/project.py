e22f0f4885e366e058c370cec374678e806a0f93
Porter <pdhakes25@gmail.com>
2026-04-23 19:11:44 -0600

    implemented Project class

--- models/project.py
+++ models/project.py
@@ -1,2 +1,29 @@
+from datetime import datetime, date
+
 class Project:
-    pass
+    _id_counter = 0
+
+    def __init__(self, title, description, due_date):
+        self.title = title
+        self.description = description
+        self.due_date = due_date
+        Project._id_counter += 1
+        self.id = Project._id_counter
+        self.tasks = []
+
+    @property
+    def due_date(self):
+        return self._due_date
+    
+    @due_date.setter
+    def due_date(self, value):
+        try:
+            self._due_date = datetime.strptime(value, "%Y-%m-%d").date()
+        except ValueError:
+            raise ValueError(f"Invalid date format: {value}. Please use YYYY-MM-DD")
+        
+    def __str__(self):
+        return f"Project #{self.id} | {self.title} | Due: {self.due_date}"
+    
+    def __repr__(self):
+        return f"Project(id = {self.id}, title = {self.title}, due_date = {self.due_date})"
