ea35a1a579e5807ea9623525414a67c1b239e35e
Porter <pdhakes25@gmail.com>
2026-04-23 19:34:02 -0600

    Created Task class

--- models/task.py
+++ models/task.py
@@ -1,2 +1,38 @@
 class Task:
-    pass
+    VALID_STATUSES = ["To Do", "In Progress", "Completed"]
+    _id_counter = 0
+
+    def __init__(self, title, assigned_to, status="To Do"):
+        self.title = title
+        self.status = status
+        self.assigned_to = assigned_to
+        Task._id_counter += 1
+        self.id = Task._id_counter
+
+    @property
+    def status(self):
+        return self._status
+    
+    @status.setter
+    def status(self, value):
+        if value not in Task.VALID_STATUSES:
+            raise ValueError(f"Status must be set to one of the following {Task.VALID_STATUSES}")
+        else:
+            self._status = value
+
+    @property
+    def assigned_to(self):
+        return self._assigned_to
+    
+    @assigned_to.setter
+    def assigned_to(self, value):
+        if not isinstance(value, str) or not value.strip():
+            raise ValueError("assigned_to must be a non-empty string")
+        else:
+            self._assigned_to = value
+
+    def __str__(self):
+        return f"Task #{self.id} | {self.title} | {self.status} | Assigned to: {self.assigned_to}"
+
+    def __repr__(self):
+        return f"Task(id={self.id}, title={self.title}, status={self.status}, assigned_to={self.assigned_to})"
--- models/user.py
+++ models/user.py
@@ -2,9 +2,11 @@ from models.person import Person
 
 class User(Person):
     _id_counter = 0
+    all = []
 
     def __init__(self, name, email):
         super().__init__(name, email)
         User._id_counter += 1
         self.id = User._id_counter
         self.projects = []
+        User.all.append(self)
