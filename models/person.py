192680a55ebf4fd5efe99b49bea383c2e143946b
Porter <pdhakes25@gmail.com>
2026-04-23 18:52:37 -0600

    implemented Person and User classes

--- models/person.py
+++ models/person.py
@@ -1,2 +1,25 @@
+from email_validator import validate_email, EmailNotValidError 
+
 class Person:
-    pass
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+
+    @property
+    def email(self):
+        return self._email
+    
+    @email.setter
+    def email(self, value):
+        try:
+            validate_email(value)
+        except EmailNotValidError:
+            raise ValueError("Invalid email address")
+        self._email = value
+
+    def __str__(self):
+        return f"{self.name} | {self.email}"
+
+    def __repr__(self):
+        return f"Person(name={self.name}, email={self.email})"
+        
